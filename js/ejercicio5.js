9094af8390e2f1c7eb211b33103ef5154250d6da
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2023-11-28 17:29:45 -0300

    Estructura y maquetado inicial

---
+++ js/ejercicio5.js
@@ -0,0 +1,4 @@
+const botonEmpezar = document.querySelectorAll('button')[0]
+const botonDetener = document.querySelectorAll('button')[1]
+const botonReiniciar = document.querySelectorAll('button')[2]
+
